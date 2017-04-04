016e428d5e1a22e84117887935a87684dd6892ac
Grady L Shelton <gradyish@gmail.com>
2017-04-03 18:44:07 -0700

    add jquery to have addresses show in website and loop through for additional websites. add addresses constructor and prototype to help accomplish this

--- js/scripts.js
+++ js/scripts.js
@@ -16,8 +16,30 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
+
 // USER INTERFACE LOGIC
 $(function() {
+  $("#add-address").click(function() {
+  $("#new-addresses").append('<div class="new-address">' +
+                               '<div class="form-group">' +
+                                 '<label for="new-street">Street</label>' +
+                                 '<input type="text" class="form-control new-street">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="new-city">City</label>' +
+                                 '<input type="text" class="form-control new-city">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="new-state">State</label>' +
+                                 '<input type="text" class="form-control new-state">' +
+                               '</div>' +
+                             '</div>');
+});
+
+
   $("form#new-contact").submit(function(e) {
     e.preventDefault();
 
@@ -26,6 +48,14 @@ $(function() {
 
     var newContact = new Contact(inputFirstName, inputLastName);
 
+    $(".new-address").each(function() {
+      var inputStreet = $(this).find("input.new-street").val();
+      var inputCity = $(this).find("input.new-city").val();
+      var inputState = $(this).find("input.new-state").val();
+      var newAddress = new Address(inputStreet, inputCity, inputState);
+      newContact.addresses.push(newAddress);
+    })
+
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
@@ -33,9 +63,16 @@ $(function() {
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+      });
     });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
   });
 });
